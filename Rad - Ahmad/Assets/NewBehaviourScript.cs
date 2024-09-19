613f357bb011c113f0a577bd64da47a744d72e16
STUDENT Ahmad Shahzad <Ahmad.Shahzad@students.ittralee.ie>
2024-09-19 10:55:05 +0100

    Signed-off-by: STUDENT Ahmad Shahzad <Ahmad.Shahzad@students.ittralee.ie>

--- Rad - Ahmad/Assets/NewBehaviourScript.cs
+++ Rad - Ahmad/Assets/NewBehaviourScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
@@ -13,6 +14,22 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.up; 
+        if(Input.GetKey(KeyCode.W))
+        transform.position += transform.forward * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(Vector3.up, 65 * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(Vector3.down, 65 * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.S))
+            transform.position += Vector3.back * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Q))
+            transform.position += Vector3.left * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.E))
+            transform.position += Vector3.right * Time.deltaTime;
     }
 }
