0fbda2836b23540aa6f0564810575256d31a76e9
STUDENT Ahmad Shahzad <Ahmad.Shahzad@students.ittralee.ie>
2024-09-25 13:04:53 +0100

    Signed-off-by: STUDENT Ahmad Shahzad <Ahmad.Shahzad@students.ittralee.ie>

---
+++ Rad - Ahmad/Assets/Physics_Cube.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Physics_Cube : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        if (rb != null)
+            print("Found Rigid Body");
+        else
+            print("Could not find Rigid Body");
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            rb.AddExplosionForce(500,
+                transform.position + new Vector3(0, -1, 0), - 1);
+
+            
+    }
+}
